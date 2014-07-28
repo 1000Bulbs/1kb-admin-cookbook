21fe2657a7f90eef19e5bab2b5229f095f204d26
Zach Campbell <zcampbell@vira-rmbp.local>
2014-07-28 14:30:42 -0500

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of 1kb-admin
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             '1kb-admin'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures 1kb-admin'
+long_description 'Installs/Configures 1kb-admin'
+version          '0.1.0'
+

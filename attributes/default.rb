09102a3498271ac5e0099f8770b71199fe31c298
daniel-jerome-long <daniel@danieljlong.com>
2015-04-06 09:01:31 -0500

    add default github users to 1kb admin cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 node.default['1kb-admin']['admin_name'] = 'admin'
-node.default['1kb-admin']['github_users'] = []
+node.default['1kb-admin']['github_users'] = ['zacharydanger' 'daniel-jerome-long', '1kb-jenkins', 'ksmeeks', 'jonathonharrell']
